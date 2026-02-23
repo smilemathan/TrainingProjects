94fb525f6f07e83710b3da03d4cbc578ae6c5144
smilemathan <smile.mathan@gmail.com>
2026-02-23 20:19:54 +0530

    #Find Nearest Number Added

---
+++ FindNearestThousand/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindNearestThousand
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number :");
+            bool isValidNumber = int.TryParse(Console.ReadLine(), out int userInput);
+            if (isValidNumber)
+            {
+                userInput= FindNearestThousand(userInput);
+                Console.WriteLine("The nearest thousand is :" + userInput);
+            }
+
+        }
+
+        public static int FindNearestThousand(int number)
+        {
+            int nearestThousand = (int)Math.Round(number / 1000.0) * 1000;
+                return nearestThousand;
+
+        }
+    }
+}
--- LoopingTutorial/Program.cs
+++ LoopingTutorial/Program.cs
@@ -10,6 +10,38 @@ namespace LoopingTutorial
     {
         static void Main(string[] args)
         {
+            //If Condtion Tutorial
+            Console.WriteLine("Enter Your Age");
+            Boolean isValidAge =int.TryParse(Console.ReadLine(),out int personAge);
+            if (isValidAge)
+            {   
+                Console.WriteLine ("You entered age is :" + personAge);
+
+                if(personAge<18)
+                {
+                    Console.WriteLine("You are not eligible to vote");
+                }
+                else if (personAge == 18)
+                {
+                    Console.WriteLine("Congratulations! You are eligible to vote for the first time");
+                }
+                else if (personAge > 18 && personAge < 100)
+                {
+                    Console.WriteLine("You are eligible to vote");
+                }
+                else
+                {
+                    Console.WriteLine("You are eligible to vote");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You entered invalid age");
+            }
+
+            Console.ReadLine();
+
+
         }
     }
 }
